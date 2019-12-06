881f953f5c694260e0dca071365fadeed1ee0b17
chito <marco3.selva@mail.polimi.it>
2019-12-06 13:49:02 +0100

    fix

--- app.js
+++ app.js
@@ -1,60 +1,68 @@
+//marco for PORT to listen
 const PORT = 3000;
-
-/* -------- IMPORT  -------- */
-//impport and create express server as an app
-const express = require('express');
+//import dependencies
+const express = require("express");
 const app = express();
-//import morgan for log in the terminal
-const morgan = require('morgan');
-//body-parser for parsin json res
-const bodyParser = require ('body-parser');
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+//import router
+const artistRoutes = require("./api/routes/artists");
+const eventRoutes = require("./api/routes/events");
+const userRoutes = require('./api/routes/user');
 
-/* -------- MAIN  -------- */
-//import artist Route
-const artistRoutes= require('./api/routes/artist');
+//connect database
+mongoose.connect(
+  "mongodb://node-shop:" +
+    process.env.MONGO_ATLAS_PW +
+    "@node-rest-shop-shard-00-00-wovcj.mongodb.net:27017,node-rest-shop-shard-00-01-wovcj.mongodb.net:27017,node-rest-shop-shard-00-02-wovcj.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin",
+  {
+    useMongoClient: true
+  }
+);
+mongoose.Promise = global.Promise;
 
-
-//log in terminal
-app.use(morgan('dev'));
-//parse url and response 
-app.use(bodyParser.urlencoded({extended: false}));
+//use parser
+app.use(morgan("dev"));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//link all routers to the app
-app.use('/artist',artistRoutes);
 
-/* -------- HANDLING CORS ERROR -------- */
-//we append some text in any response header to fix this bug
+//CORS error correction
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
-app.use((req, res, next)=>{
-    //instead of * can be the only website can use API
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization");
-    if(req.method === 'OPTIONS'){
-        res.header("Access-Control-Allow-Method", "PUT","POST","PATCH","DELETE","GET");
-        return res.status(200).json({});
+// Routes which should handle requests
+app.use("/artists", artistRoutes);
+app.use("/events", eventRoutes);
+app.use("/semiar", seminarRoutes);
+app.use("/user", userRoutes);
+
+//handler the 404 error
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+//handler all error or generic 500 error
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
     }
-    next();
+  });
 });
-
-/* -------- ERROR CONTROLL -------- */
-//error 404 for not found resources
-app.use((req,res,next)=>{
-    const error = new Error ('Not found');
-    error.status = 404;
-    next(error);
-    
-})
-
-//catch alla error from app, or general 500 error
-app.use((error,req,res,next)=>{
-    res.status(error.status || 500);
-    res.json ({
-        error:{
-            message: error.message
-        }
-    });
-});
-
 
 //app is listening for request on PORT
 app.listen(PORT, () => {

a36bac574ba37d497708b3a551cb6b19ee702a38
Andrea Tresoldi <andrea3.tresoldi@mail.polimi.it>
2019-12-18 15:05:04 +0100

    prova

---
+++ assets/js/artists.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$.get("http://localhost:3000/artist", function(data, status){
+
+		var jsonDataArtists=JSON.parse(data);
+		console.log(jsonDataArtists);
+
+
+	}
+
+});

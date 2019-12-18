71a1bdd6daeb79d5ac3bc1643d9329616f4dfe91
chito <marco3.selva@mail.polimi.it>
2019-12-18 16:07:04 +0100

    fix

--- assets/js/artists.js
+++ assets/js/artists.js
@@ -2,50 +2,28 @@ $(document).ready(function(){
 
 	$.get("http://localhost:3000/artist", function(data, status){
 
-		
-		//console.log(data.artists[0]);
-		//console.log(data.artists[0].name);
-		//const ciao = JSON.parse(data);
-
-		const art= JSON.parse(data);
-
-		art.artists.forEach(element => {
-			console.log(element.name);
-		});
+		const jsonDataArtists= JSON.parse(data);
 
 		for (var i =0;i<jsonDataArtists.lenght;i++){
 
 			$("#performer").append(
-
-				`
-				<div class="col-md-4 col-6 mb-4">   
-					<a href="singleartist.html">  
-						<img src="../../${jsonDataArtists[i].photoGallery}" class="imagesArtist">                   
-					</a> 
-					<div class="artista"><h5><b>ROBERTO BOLLE</b></h5></div>
-					<h7 class="type"><i><b>dancer</b></i></h7>
-				</div> 
-				
-				`
-			);
-				
-
-
-
-
-				
-
-
-
-
-
-
-
-
-	
+			`
+			<div class="col-md-4 col-6 mb-4">   
+				<a href="singleartist.html">  
+					<img src="" class="imagesArtist">                   
+				</a> 
+				<div class="artista">
+					<h5>
+						<b>${jsonDataArtists[i].name}</b>
+					</h5>
+				</div>
+				<h7 class="type"><i>
+					<b>${jsonDataArtists[i].type}</b>
+					</i>
+				</h7>
+			</div> 
+			`
+		);
 		}
-
-
 	});
-
 });

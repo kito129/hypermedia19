4ab0e4c61a326d329494a39601d0f925524c3855
Andrea Tresoldi <andrea3.tresoldi@mail.polimi.it>
2019-12-18 15:29:51 +0100

    update

--- assets/js/artists.js
+++ assets/js/artists.js
@@ -5,6 +5,38 @@ $(document).ready(function(){
 		var jsonDataArtists=JSON.parse(data);
 		console.log(jsonDataArtists);
 
+		for (var i =0;i<jsonDataArtists.lenght;i++){
+
+			$("#performer").append(
+
+				`
+				<div class="col-md-4 col-6 mb-4">   
+					<a href="singleartist.html">  
+						<img src="../images/private/artists/bolle.jpg" class="imagesArtist">                   
+					</a> 
+					<div class="artista"><h5><b>ROBERTO BOLLE</b></h5></div>
+					<h7 class="type"><i><b>dancer</b></i></h7>
+				</div> 
+				
+				`
+			);
+				
+
+
+
+
+				
+
+
+
+
+
+
+
+
+	
+		}
+
 
 	});
 

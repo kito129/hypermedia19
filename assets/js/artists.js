3d3bce6945a6758f3a9806d249c7fe75934ccbb4
Andrea Tresoldi <andrea3.tresoldi@mail.polimi.it>
2019-12-18 17:37:20 +0100

    update

--- assets/js/artists.js
+++ assets/js/artists.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 				`
 				<div class="col-md-4 col-6 mb-4">   
 					<a href="singleartist.html">  
-						<img src="" class="imagesArtist">                   
+						<img src="../../${jsonArtists.artists[i].photoGallery}"class="imagesArtist">                   
 				 	</a> 
 					<div class="artista"><h5><b>${jsonArtists.artists[i].name}</b></h5></div>
 					<h7 class="type"><i><b>${jsonArtists.artists[i].type}</b></i></h7>

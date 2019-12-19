7c5791530e359840013c2fdb64ab5e132fffcb08
Andrea Tresoldi <andrea3.tresoldi@mail.polimi.it>
2019-12-19 15:07:44 +0100

    update

---
+++ assets/js/singleArtist.js
@@ -0,0 +1,30 @@
+function getUrlParameterValue(url, parameter) {
+    var questionSplit = url.split('?');
+    questionSplit.shift();
+    var onlyParameters = questionSplit.join('?');
+    var splittedParameters = onlyParameters.split('&');
+    var found = false;
+    var value = null;
+    for (var c = 0; c < splittedParameters.length; c++) {
+        var parts = splittedParameters[c].split('=');
+        if (parts[0] == parameter) {
+            value = parts[1];
+            if (value.trim()== '') {
+                found = false;
+            } else {
+                found = true;
+            }
+        }
+        if (found) {
+            return value;
+        }
+    }
+    if (!found) {
+        return false;
+    }
+}
+
+var id=getUrlParameterValue(self.location.href,"id");
+console.log(id);
+
+

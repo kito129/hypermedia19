fd304951ec4a379016ab1002d30b7878174140f9
chito <marco3.selva@mail.polimi.it>
2019-12-20 22:45:59 +0100

    fix

--- api/models/artist.js
+++ api/models/artist.js
@@ -4,9 +4,9 @@ const artistSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true },
     currentAffiliattion: { type: String,  },
-    achivements: { type: [],},
+    achivements: { type: Array},
     isCompany: { type: Boolean, default: false },
-    companyMembers: { type: [], required: false },
+    companyMembers: { type: Array, required: false },
     abstract: { type: String, required: false },
     type:{ type: String, required: true },
     photoGallery: { type: String, required: false }

284cc45840a4d5c951bb02310a2e1af2beb23ab2
Andrea Tresoldi <andrea3.tresoldi@mail.polimi.it>
2019-12-21 20:55:53 +0100

    update

--- public/assets/js/singleArtist.js
+++ public/assets/js/singleArtist.js
@@ -28,10 +28,11 @@ function getUrlParameterValue(url, parameter) {
 $(document).ready(function(){
 
     var idArtist=getUrlParameterValue(self.location.href,"id");
+    var jsonArtist;
     $.get("https://hypermedia19.herokuapp.com/artist/"+idArtist, function(data, status){
 
-        var jsonArtist=JSON.parse(data);
-        //console.log(jsonArtist.artist);
+        jsonArtist=JSON.parse(data);
+        
 
 
         $("#singoloArtista").append(
@@ -58,7 +59,7 @@ $(document).ready(function(){
 
             for(var i=0;i<jsonArtist.artist.achivements.length;i++){
 
-                text=text+jsonArtist.artist.achivements[i]+"---";
+                text=text+jsonArtist.artist.achivements[i]+"--";
 
             }
 
@@ -76,42 +77,94 @@ $(document).ready(function(){
 
             `
             <a class="currentAffiliation">Current Affiliation: <b>${jsonArtist.artist.currentAffiliattion}</b></a><br>
-            <a class="date">Company Members: <b>-------------------------ciao</b></a><br>
+
+            `
+            );
+
+        if(jsonArtist.artist.isCompany==true){
+
+            var members="";
+
+            for(var i=0;i<jsonArtist.artist.companyMembers.length;i++){
+
+                members=members+jsonArtist.artist.companyMembers[i]+"--";
+
+            }
+
+            $("#singleEvent").append(
+
+            `
+            <a class="date">Company Members: <b id="members"></b></a><br>
+            
+
+            `
+            );
+
+            $("#members").text(members);
+
+        }
+
+        $("#singleEvent").append(
+
+            `
+
             <div style="text-align:justify" class="paragrafofullpage">
                 <p class="descrizione"><b>${jsonArtist.artist.abstract}</b></p>
             </div>
+           
 
-
-              `
+            `
             );
+
+
     });
 
-/*
     $.get("https://hypermedia19.herokuapp.com/event/", function(data, status){
 
         var jsonEvents=JSON.parse(data);
-        console.log(jsonEvents.events.length);
 
-        for(var i=0;i<jsonEvents.events.lenght;i++){
 
-            
-          
-            if(){
+        for(var i=0;i<jsonEvents.events.length;i++){
 
-                $("singoloEvento").append(
+            if(jsonEvents.events[i].artistId==idArtist){
+
+                
+                $("#fotoEvento").append(
 
                     `
-                    <img src="../../../${jsonEvents[i].photoGallery}" class="imageSingleEvent">
+                    <img src="../../../${jsonEvents.events[i].photoGallery}" class="imageSingleEvent">
 
                     `
 
                     );
+                $("#infoEvent").append(
+
+                    //da finire il riferimento all'evento singolo
+
+                     `
+                    <a class="name" href="singleevent.html"><b>SEVEN DAYS WALKING TOUR</b></a><br> 
+                    <a class="artista"><b>${jsonArtist.artist.name}</b></a><br>
+                    <a class="date">Date: <b>12/06/2020 - 21.00</b></a><br>
+                    <a class="place">Place: <b>Place of Tree of Life</b></a><br>
+                    <a class="price">Price: <b>50.00€</b></a><br>
+                    <span>
+                        <button type="button" class="btn btn-dark btncart"><b>-</b></button>
+                        <span class="quantity"><b>3</b></span>
+                        <b>TICKETS&ensp;</b>
+                        <button type="button" class="btn btn-dark btncart"><b>+</b></button>
+                        <button type="button" class="btn btn-dark" disabled><b>SOLD OUT</b></button><br>
+                    </span>
+
+                      `
+
+                      );
+                    
             }
             
         }
     });
     
-    */
+    
 
 
 

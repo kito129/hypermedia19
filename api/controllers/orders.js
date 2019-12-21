cf5b6a308b18606dd9a6db2b08ef9d349315aa93
chito <marco3.selva@mail.polimi.it>
2019-12-21 21:29:38 +0100

    fix seminr

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -3,24 +3,22 @@ const mongoose = require("mongoose");
 const Order = require("../models/orders");
 const Event = require("../models/event");
 
-
-//TODO
+//OK
 exports.orders_get_all = (req, res, next) => {
   Order.find()
-    .select("_id userId order totalPrice")
+    .select("_id userId order eventId totalPrice")
     .exec()
     .then(docs => {
       const response = {
-        count: docs.length,
         orders: docs.map(doc => {
           return {
             userId: doc.userId,
-            order: doc.order,
+            order: doc.order[0],
             totalPrice: doc.totalPrice,
             _id: doc._id,
             request: {
               type: "GET",
-              url: "https://hypermedia19.herokuapp.com/order/" + doc._id
+              url: "https://hypermedia19.herokuapp.com/order/" + doc._id,
             }
           };
         })
@@ -41,7 +39,7 @@ exports.orders_get_all = (req, res, next) => {
     });
 };
 
-
+//OK
 exports.orders_create_order = (req, res, next) => {
   const body = req.body;
   const bodyOrder = body.order;
@@ -87,20 +85,6 @@ exports.orders_create_order = (req, res, next) => {
                 }
               });
             })
-            .catch(err => {
-              if(err.name="CastError"){
-                console.log("Invalid eventId input:\n" + err);
-                res.status(400).json({
-                  message: "Invalid eventId input",
-                  error: err
-                });
-              } else {
-                console.log("ERROR:\n" + err);
-                res.status(500).json({
-                  error: err
-                });
-              }
-            });
         })
         .catch(err => {
           if(err.name="CastError"){
@@ -134,70 +118,105 @@ exports.orders_create_order = (req, res, next) => {
    
   };
 
-  //CHECK
+//OK
 exports.orders_get_order = (req, res, next) => {
   const id = req.params.orderId;
   Order.findById(id)
     .select("_id userId order totalPrice")
     .exec()
     .then(doc => {
-      if (doc) {
-        res.status(200).json({
-          order: doc,
-          request: {
-            type: "GET",
-            url: "https://hypermedia19.herokuapp.com/order/"
-          }
-        });
-      } else {
-        res
-          .status(404)
-          .json({ message: "provided ID order NOT FOUND" });
-      }
+      res.status(200).json({
+        order: doc,
+        request: {
+          type: "GET",
+          url: "https://hypermedia19.herokuapp.com/order/"
+        }
+      });
     })
     .catch(err => {
-      console.log("ERROR:\n" + err);
-      res.status(500).json({ error: err });
+      if(err.name="CastError"){
+        console.log("ERROR:\n" + "provided ID order NOT FOUND");
+        res.status(404).json({ message: "provided ID order NOT FOUND" });
+      } else{
+        console.log("ERROR:\n" + err);
+        res.status(500).json({ error: err });
+      }
     });
 };
 
 
 //TODO
 //inserimento nuoi biglietti o modifca dal carrello
+
+//se get:id da 404 allora creo il carrello con il primo ordine.
+//nel se restituisce 200 allora 
+  //mi da parametro lui se esiste evento 
+    //controllo se c'e gia event ID
+          //if yes => aggiorno quantità * sub e totale (se nuova=0 allora cancello ordine (se size==0 allora cancello l'rodine completo pure))
+          //if no => aggiungo ad arrraya nuovo elemento con evento e quantita
+              //calcolo sub e nuovo totale
+
+
 exports.orders_update_order = (req, res, next) => {
   const id = req.params.orderId;
-  const updateOps = {};
-  for (const ops of req.body ) {
-    updateOps[ops.propName] = ops.value;
-  }
-  var options = {new: true};
-  Order.findOneAndUpdate({ _id: id }, { $set: updateOps },options,function (err, doc) {  
-  })
-    .exec()
-    .then(result => {
-      console.log(result);
-      res.status(200).json({
-        message: "Order updated",
-        request: {
-          type: "GET",
-          url: "https://hypermedia19.herokuapp.com/order/" + id
+  const type = req.params.update;
+  const body = req.body;
+  console.log(body);
+  if(type==="true"){
+    //type true se c'e gia evento e evi aggioranre quantita 
+
+    const updateOps = {};
+    for (const ops of req.body ) {
+      updateOps[ops.propName] = ops.value;
+      console.log(updateOps[ops.propName]);
+      console.log(updateOps[ops.propName]);
+    }
+    var options = {new: true};
+
+    Order.findOneAndUpdate({ _id: id }, { $set: updateOps },options,function (err, doc) {})
+      .exec()
+      .then(result => {
+        console.log(result);
+        res.status(200).json({
+          message: "Order updated",
+          request: {
+            type: "GET",
+            url: "https://hypermedia19.herokuapp.com/order/" + id
+          }
+        });
+      })
+      .catch(err => {
+        if(err.name="CastError"){
+          console.log("Order ID not found");
+          res.status(404).json({
+            error: "Order ID not found"
+          });
+        } else{
+          console.log("ERROR:\n" + err);
+          res.status(500).json({
+            error: err
+          });
         }
       });
-    })
-    .catch(err => {
-      if(err.name="CastError"){
-        console.log("Order ID not found");
-        res.status(404).json({
-          error: "Order ID not found"
-        });
-      } else{
-        console.log("ERROR:\n" + err);
-        res.status(500).json({
-          error: err
-        });
-      }
-    });
+  
+
+    
+  } else {
+    //se non c'e evento ed è da aggiungere al carrelo
+
+  }
+
+  
+
 };
+
+
+
+
+
+
+
+
 
 
 //OK
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -11,7 +11,7 @@ router.post("/", checkAuth, OrdersController.orders_create_order);
 
 router.get("/:orderId", checkAuth, OrdersController.orders_get_order);
 
-router.patch("/:orderId", checkAuth, OrdersController.orders_update_order);
+router.patch("/:orderId/:update", checkAuth, OrdersController.orders_update_order);
 
 router.delete("/:orderId", checkAuth, OrdersController.orders_delete);
 

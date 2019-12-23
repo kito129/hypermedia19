28c9ff43fe8de9e59b45f733959a41b97f8120ea
chito <marco3.selva@mail.polimi.it>
2019-12-23 12:14:15 +0100

    fix4

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -9,6 +9,6 @@ module.exports = (req, res, next) => {
     } catch (error) {
         return res.status(401).json(JSON.Stringify({
             message: 'Auth failed'
-        });
+        }));
     }
 };

08ec15b6b1ac45cb1b4703e4795986e33838e3ac
chito <marco3.selva@mail.polimi.it>
2020-01-06 11:47:22 +0100

    fix order db

--- api/models/orders.js
+++ api/models/orders.js
@@ -3,13 +3,7 @@ const mongoose = require('mongoose');
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    order: [
-        {
-            eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
-            quantity: {type: Number, required: true},
-            subTotal: {type: Number, required: false}
-        }
-    ],
+    order: { type: mongoose.Schema.Types.ObjectId, ref: 'SingleOrder', required: true },
     totalPrice: {type: Number, required: false}
 });
 

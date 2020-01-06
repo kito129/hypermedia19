df67e89f7e947d3d8f13b98a9a66042bb563780e
chito <marco3.selva@mail.polimi.it>
2020-01-06 14:21:46 +0100

    fix

--- api/models/orders.js
+++ api/models/orders.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const orderSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    order: { type: mongoose.Schema.Types.ObjectId, ref: 'SingleOrder', required: true },
+    SinlgeOrder: { type: mongoose.Schema.Types.ObjectId, ref: 'SingleOrder', required: true },
     totalPrice: {type: Number, required: false}
 });
 
